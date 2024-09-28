832e20b72dbc354cbbb832c33ff77dfc7031d59b
Ayrat Kalimov <ayrat.klim1@yandex.ru>
2024-09-28 23:47:11 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MyComp1 from "./mycomp1";
+
+function App() {
+  return (
+    <>
+      <MyComp1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/mycomp1/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+
+const MyComp1 = () => {
+  return (
+    <div>
+      <p className={styles.text}>
+        <p className={styles.text2}>Текст</p>
+      </p>
+    </div>
+  );
+};
+
+export default MyComp1;
